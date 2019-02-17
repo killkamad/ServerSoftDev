214f6c88cd9b13298f77069af24687785dec35a3
Kirill <killka_m@mail.ru>
2019-02-17 19:55:16 +0600

    da

---
+++ mysite2/students/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class Publication(models.Model):
+    title = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ('title',)
+
+class Article(models.Model):
+    headline = models.CharField(max_length=100)
+    publications = models.ManyToManyField(Publication)
+
+    def __str__(self):
+        return self.headline
+
+    class Meta:
+        ordering = ('headline',)
