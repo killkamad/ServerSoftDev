e158125ac27a891560f32d38832ffb2871223306
Kirill <killka_m@mail.ru>
2019-02-19 17:06:22 +0600

    add

--- mysite3/almau/admin.py
+++ mysite3/almau/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Person, Group
+from .models import Person, Group, Membership
 
 admin.site.register(Person)
 admin.site.register(Group)
+admin.site.register(Membership)
 
