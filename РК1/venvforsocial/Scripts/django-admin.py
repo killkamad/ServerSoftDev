4cfd1570910db574f0d984d16ec70cae6d5fdd17
Kirill <killka_m@mail.ru>
2019-03-05 00:06:57 +0600

    removed

--- РК1/venvforsocial/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!f:\универ\программирование\github\serversoftdev\рк1\venvforsocial\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
